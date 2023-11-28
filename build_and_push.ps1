2754ff005c0a7625e750e0a5509dc7c3b01b114e
Nyirsh <nyirsh@outlook.com>
2023-11-28 15:53:20 -0600

    Improved self build scripts

--- build_and_push.ps1
+++ build_and_push.ps1
@@ -2,7 +2,7 @@
 # Run this commented command once if you never initalized a buildx container before
 # docker buildx create --use --name build --node build --driver-opt network=host
 
-PIHOLE_VER=$(cat VERSION)
+$PIHOLE_VER = Get-Content -Raw VERSION
 
 docker buildx build --build-arg PIHOLE_VERSION=$PIHOLE_VER --pull --rm -f "Dockerfile" --platform linux/arm/v7,linux/arm64/v8,linux/amd64 -t nyirsh/pihole-unbound:$PIHOLE_VER "pihole-unbound" --push
 docker buildx build --build-arg PIHOLE_VERSION=$PIHOLE_VER --pull --rm -f "Dockerfile" --platform linux/arm/v7,linux/arm64/v8,linux/amd64 -t nyirsh/pihole-unbound:latest "pihole-unbound" --push
